2bae20ea264f45c411e857680d7562750337760a
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-26 20:13:23 +0700

    feat: Add `app-config.ts` with `getBaseUrl` function to retrieve API URL from global environment or fallback.

--- src/app/core/config/app-config.ts
+++ src/app/core/config/app-config.ts
@@ -1,10 +1,6 @@
 import { environment } from '../../../environments/environment';
 
 export function getBaseUrl(): string {
-  // Lấy môi trường từ window do Shell App truyền xuống
   const shellEnv = (window as any).__env;
-
-  // Nếu chạy trong Shell App và có apiUrl -> dùng url của Shell
-  // Nếu chạy độc lập (ng serve auth-app) -> dùng url trong environment.ts
   return shellEnv && shellEnv.apiUrl ? shellEnv.apiUrl : environment.apiUrl;
 }
