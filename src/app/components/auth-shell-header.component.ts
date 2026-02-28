6868236ea399973b89acb0067501f976bc493bd7
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2026-02-28 10:38:05 +0700

    feat: create AuthShellHeaderComponent.

--- src/app/components/auth-shell-header.component.ts
+++ src/app/components/auth-shell-header.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-auth-shell-header',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './auth-shell-header.component.html',
   styleUrls: ['./auth-shell-header.component.scss'],
 })
